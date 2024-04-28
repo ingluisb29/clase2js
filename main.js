864be9f65dbed317565d72901b2c6aae96f2cafe
luis <luiso.bricenov@gmail.com>
2024-04-28 12:11:30 -0300

    actualizar

--- main.js
+++ main.js
@@ -17,9 +17,40 @@
 // sugarsintax i++ 
 
 
+// dubegger es limpiar los problemas del sistema
+
+//debugger
+
+/*let i = parseInt(prompt("ingrese un numero"))
 
 for( let i = 0; i<10; i++){
-    alert(i)
-}
+    alert("hola javascript" + " " + i)
+}*/
 
 //let i =0
+
+/* let numero = parseInt(prompt("ingrese un numero"))
+
+for(let i=1; i<10; i++){
+
+    let resultado = numero *i
+    alert("esta es la tabla del numero " + numero + " " + resultado)
+}*/
+
+// el break es un condicional que hace que el for corte cuando es falsa la condicion dentro del break
+// el continue salta la condicion que uno coloque
+
+/*for(let i=1; i<10; i++){
+    // si la variable i es igual 5 interrumpo el for
+    if( i == 5){
+        continue;
+    }
+
+    alert(i)
+
+
+}*/
+
+// el bucle for se utiliza cuando sabemos el numero de veces que queremos que se repita una condicion
+// el ciclo while se repite mientras la condicion sea verdadera
+
