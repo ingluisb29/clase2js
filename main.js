4fed8367900d9a02ea98ac6f5b42dc64c8419c9d
luis <luiso.bricenov@gmail.com>
2024-04-28 12:09:03 -0300

    practica gift con clase3

---
+++ main.js
@@ -0,0 +1,25 @@
+//tipos de bucles 
+//ciclos por conteo
+// estructura for
+//repite un codigo un numero de veces especificas
+// for (let i = 0; i < 10; i++){
+// alert(i);
+//}
+
+//ciclos condicionales
+// estructura while o do while
+// repite la condicion mientras sea verdadera
+
+// for
+
+// desde; hasta; actualizacion/incrementos
+// i++ quiere decir que va de uno en uno
+// sugarsintax i++ 
+
+
+
+for( let i = 0; i<10; i++){
+    alert(i)
+}
+
+//let i =0
